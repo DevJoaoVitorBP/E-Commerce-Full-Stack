db07e97c3c519571d95d7590bf33f6fdceeba851
DevJoaoVitorBP <joaov.batalha013@hotmail.com>
2026-07-21 11:15:02 -0300

    fix: change background color of "Produtos em Destaque" section to white to fix color issue on phone in dark mode

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -69,7 +69,7 @@
     </section>
 
     <!-- Produtos em Destaque -->
-    <section class="py-20 bg-gray-50">
+    <section class="py-20 bg-white">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex items-end justify-between mb-10">
           <div>
